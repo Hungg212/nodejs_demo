1663cc13540520242278c4a7efb6f12cc32e7322
Nguyen Le Hung <lehungnguyen223@gmail.com>
2025-09-13 17:04:02 +0700

    First commit - Chương trình NodeJS hiển thị tên và tuổi

---
+++ index.js
@@ -0,0 +1,9 @@
+// Hàm hiển thị tên và tuổi 
+function displayInfo(name,age){
+    console.log(`Họ tên:${name}`);
+    console.log(`Tuổi:${age}`);
+}
+
+// Gọi hàm với dữ liệu mẫu
+displayInfo("Nguyễn Văn A", 25);
+displayInfo("Trần Thị B", 30);
